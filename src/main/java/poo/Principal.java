42474fc99dca1de12c12e499d6bcd0f5c5bc7dbc
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-04-16 16:24:59 -0300

    Testes realizados para validação dos números que serão add. Método de verificação está OK.

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -1,4 +1,24 @@
 package poo;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Telefone telefone = new Telefone();
+        String nome;
+        String numero;
+
+        System.out.println("Digite o nome e o número a serem adicionados:");
+        nome = teclado.next();
+        numero = teclado.next();
+
+        if(telefone.add(nome,numero)){
+            System.out.println("Dados corretos!");
+        }
+
+        else{
+            System.out.println("Dados incorretos!");
+        }
+    }
 }
--- src/main/java/poo/Telefone.java
+++ src/main/java/poo/Telefone.java
@@ -1,4 +1,71 @@
 package poo;
 
+import java.util.HashMap;
+
 public class Telefone {
+
+    private HashMap <String,String> dadosTelefone = new HashMap<>();
+
+    public Telefone() {
+    }
+
+    public boolean add(String rotulo, String numero){
+        if (verificador(numero)){
+            dadosTelefone.put(rotulo, numero);
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+
+//    public boolean remove(String rotulo){
+//
+//    }
+//
+//    public boolean update(String rotulo, String nome){
+//
+//    }
+
+    public boolean verificador(String numero){
+        int i;
+        for (i = 0; i < numero.length(); i++) {
+            if ((numero.charAt(i) < '0') || (numero.charAt(i) > '9')){
+                return false;
+            }
+        }
+
+        if (i == numero.length()){
+            if (numero.length() == 11) {
+                if (numero.charAt(3) == '3') {
+                    if (numero.charAt(0) == '0') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+
+            else if (numero.length() == 12){
+                if (numero.charAt(3) == '9'){
+                    if (numero.charAt(0) == '0')
+                    {
+                        return true;
+                    }
+
+                    else{
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefone{}";
+    }
 }
