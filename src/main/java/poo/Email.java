e88e2489521e92bbef6d4085e4bd58f55345e08f
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-04-18 19:12:15 -0300

    Criado os métodos da classe Email.

--- src/main/java/poo/Email.java
+++ src/main/java/poo/Email.java
@@ -1,4 +1,37 @@
 package poo;
 
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.HashMap;
+
 public class Email {
+    private HashMap<String,String> dadosEmail = new HashMap<>();
+
+//    public Email() {
+//    }
+//
+//    public boolean add(String rotulo, String email){
+//
+//    }
+//
+//    public boolean remove(String rotulo){
+//
+//    }
+//
+//    public boolean update(String rotulo, String email){
+//
+//    }
+//
+//    public boolean verificador(String email){
+//
+//    }
+//
+//    public String formata(String email){
+//
+//    }
+//
+//    @Override
+//    public String toString() {
+//
+//    }
 }
